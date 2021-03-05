e7472b27039d7e176d3f9ffcc808367c3c0da436
AnastasiiaGanieva <69317903+AnastasiiaGanieva@users.noreply.github.com>
2021-03-05 23:52:53 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
-public class Main{
-public static void main(String[] args){
-System.out.println("Hello world!");
-}
+public class Main {
+    public static void main(String[] args) {
+        int score = 2_000_000_000;
+        int sum = 500_000_000;
+        int total = score + sum;
+        System.out.println("Total:"+ total);
+    }
 }
